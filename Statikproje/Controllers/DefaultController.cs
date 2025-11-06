89cc145e87173f50bff29c20d3822182bd3e784b
denizdogruel00 <denizyazilm@gmail.com>
2025-11-06 11:36:32 +0300

    statik proje eklendi

---
+++ Statikproje/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Statikproje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
